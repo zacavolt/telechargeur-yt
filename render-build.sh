10a9f348324364275c432c03083f21eb79d0d9b2
zacavolt <pourquoi.pas.moi.100@gmail.com>
2025-07-15 11:01:06 +0200

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
-mkdir -p videos
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
-chmod +x /usr/local/bin/yt-dlp
+#!/bin/bash
+
+mkdir -p bin
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o bin/yt-dlp
+chmod +x bin/yt-dlp
+
